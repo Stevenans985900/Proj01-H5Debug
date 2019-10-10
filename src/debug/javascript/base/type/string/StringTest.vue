098fd620f3c1a36c9b590ddaeb858024826abcee
zhengoogle <zhengoogle@sina.com>
2019-10-11 01:26:17 +0800

    es5 feature;

---
+++ src/debug/javascript/base/type/string/StringTest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="zz-page"></div>
+</template>
+
+<script>
+/**
+ * 字符串模板
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/template_strings
+ */
+export default {
+  name: 'StringTest'
+}
+</script>
+
+<style scoped>
+
+</style>
