60b5093bb0bd4f1cdc70685d7f50ea3420012678
zhengoogle <zhengoogle@sina.com>
2020-03-11 22:14:02 +0800

    up

--- src/debug/html/label/input/InputTest.vue
+++ src/debug/html/label/input/InputTest.vue
@@ -1,8 +1,21 @@
 <template>
-    <div></div>
+    <div class="zz-page"></div>
 </template>
 
 <script>
+/**
+ * html/input
+ * --- AA
+ *
+ * Router
+ * --- http://localhost:8801/#/html/input
+ *
+ * issue
+ * #1.input type="number"时 maxlength 属性不起作用
+ * --- 规避方案：使用tel
+ * --- https://blog.csdn.net/u012982629/article/details/80588438
+ */
+
 export default {
   name: 'InputTest'
 }
