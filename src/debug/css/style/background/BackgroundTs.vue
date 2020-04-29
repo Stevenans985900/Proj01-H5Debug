2c87549e9f24330292d0d1ddc2cee42bbde7afa3
zhengoogle <zhengoogle@sina.com>
2020-04-29 23:20:19 +0800

    transform-3d

--- src/debug/css/style/background/BackgroundTs.vue
+++ src/debug/css/style/background/BackgroundTs.vue
@@ -11,6 +11,8 @@
  * --- background-image: linear-gradient(#e66465, #9198e5); // 从上到下的线性渐变
  * --- background-image: linear-gradient(to right, red , yellow); // 从左到右的线性渐变
  * --- https://www.runoob.com/css3/css3-gradients.html
+ * ---背景半透明
+ * --- background: rgba(0, 0, 0, 0.5);
  *
  * Router
  * --- http://localhost:8801/#/html/verify
